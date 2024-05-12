01cd1bc81c4b1ad3d029e9df433702eb5fe718cd
boybeak <boybeak@gmail.com>
2024-05-12 17:15:54 +0800

    feat: ui init

--- MyTodo-Mac/MyTodo/AppDelegate.swift
+++ MyTodo-Mac/MyTodo/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 class Tray {
     
-    public static let POPOVER_SIZE = NSSize(width: 320, height: 400)
+    public static let POPOVER_SIZE = NSSize(width: 400, height: 400)
     
     private let iconName: String
     private var statusItem: NSStatusItem!
--- MyTodo-Mac/MyTodo/ViewController.swift
+++ MyTodo-Mac/MyTodo/ViewController.swift
@@ -8,29 +8,57 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    private static let EDITOR_HEIGHT = CGFloat(32)
+    private static let SEPARATOR_COLOR = NSColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1)
+    
+    private var navigator: NSScrollView!
+    
+    private let navigatorDataSource = NavigatorDataSource()
+    private let navigatorDelegate = NavigatorDelegate()
+    
+    private let tableDataSource = TableDataSource()
+    private let tableDelegate = TableDelegate()
+    
+    private var selectedButton: NSButton?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let container = NSStackView()
-        container.orientation = .vertical
+        container.orientation = .horizontal
         container.translatesAutoresizingMaskIntoConstraints = false
+        
+        let noteContainer = NSStackView()
+        noteContainer.orientation = .vertical
+        noteContainer.translatesAutoresizingMaskIntoConstraints = false
 
-        let navigator = NSView()
+        navigator = makeNavigator()
         navigator.wantsLayer = true
         navigator.layer?.backgroundColor = NSColor.blue.cgColor
         
-        let content = NSView()
+        container.addArrangedSubview(navigator)
+        
+        let separator1 = NSView()
+        separator1.wantsLayer = true
+        separator1.layer?.backgroundColor = ViewController.SEPARATOR_COLOR.cgColor
+        
+        container.addArrangedSubview(separator1)
+        container.addArrangedSubview(noteContainer)
+        
+        let content = makeList()
         content.wantsLayer = true
         content.layer?.backgroundColor = NSColor.orange.cgColor
         
-        let editor = NSView()
-        editor.wantsLayer = true
-        editor.layer?.backgroundColor = NSColor.purple.cgColor
+        let separator2 = NSView()
+        separator2.wantsLayer = true
+        separator2.layer?.backgroundColor = ViewController.SEPARATOR_COLOR.cgColor
         
-        container.addArrangedSubview(navigator)
-        container.addArrangedSubview(content)
-        container.addArrangedSubview(editor)
+        let editor = makeEditor()
+        
+        noteContainer.addArrangedSubview(content)
+        noteContainer.addArrangedSubview(separator2)
+        noteContainer.addArrangedSubview(editor)
         
         self.view.addSubview(container)
         
@@ -43,22 +71,120 @@ class ViewController: NSViewController {
         
         NSLayoutConstraint.activate([
             navigator.leadingAnchor.constraint(equalTo: container.leadingAnchor),
-            navigator.trailingAnchor.constraint(equalTo: container.trailingAnchor),
             navigator.topAnchor.constraint(equalTo: container.topAnchor),
-            navigator.heightAnchor.constraint(equalToConstant: 40)
+            navigator.bottomAnchor.constraint(equalTo: container.bottomAnchor),
+            navigator.widthAnchor.constraint(equalToConstant: 120)
+        ])
+        
+        NSLayoutConstraint.activate([
+            separator1.leadingAnchor.constraint(equalTo: navigator.trailingAnchor),
+            separator1.topAnchor.constraint(equalTo: container.topAnchor),
+            separator1.bottomAnchor.constraint(equalTo: container.bottomAnchor),
+            separator1.widthAnchor.constraint(equalToConstant: 0.5)
+        ])
+        
+        NSLayoutConstraint.activate([
+            noteContainer.leadingAnchor.constraint(equalTo: separator1.trailingAnchor),
+            noteContainer.trailingAnchor.constraint(equalTo: container.trailingAnchor),
+            noteContainer.topAnchor.constraint(equalTo: container.topAnchor),
+            noteContainer.bottomAnchor.constraint(equalTo: container.bottomAnchor)
         ])
         NSLayoutConstraint.activate([
-            editor.leadingAnchor.constraint(equalTo: container.leadingAnchor),
-            editor.trailingAnchor.constraint(equalTo: container.trailingAnchor),
-            editor.bottomAnchor.constraint(equalTo: container.bottomAnchor),
-            editor.heightAnchor.constraint(equalToConstant: 40)
+            editor.leadingAnchor.constraint(equalTo: noteContainer.leadingAnchor),
+            editor.trailingAnchor.constraint(equalTo: noteContainer.trailingAnchor),
+            editor.bottomAnchor.constraint(equalTo: noteContainer.bottomAnchor),
+            editor.heightAnchor.constraint(equalToConstant: ViewController.EDITOR_HEIGHT)
         ])
         NSLayoutConstraint.activate([
-            content.leadingAnchor.constraint(equalTo: container.leadingAnchor),
-            content.trailingAnchor.constraint(equalTo: container.trailingAnchor),
-            content.topAnchor.constraint(equalTo: navigator.bottomAnchor),
-            content.bottomAnchor.constraint(equalTo: editor.topAnchor)
+            separator2.leadingAnchor.constraint(equalTo: noteContainer.leadingAnchor),
+            separator2.trailingAnchor.constraint(equalTo: noteContainer.trailingAnchor),
+            separator2.bottomAnchor.constraint(equalTo: editor.topAnchor),
+            separator2.heightAnchor.constraint(equalToConstant: 0.5)
+        ])
+        NSLayoutConstraint.activate([
+            content.leadingAnchor.constraint(equalTo: noteContainer.leadingAnchor),
+            content.trailingAnchor.constraint(equalTo: noteContainer.trailingAnchor),
+            content.topAnchor.constraint(equalTo: noteContainer.topAnchor),
+            content.bottomAnchor.constraint(equalTo: separator2.topAnchor)
         ])
     }
+    
+    private func makeNavigator()-> NSScrollView {
+        let scrollView = NSScrollView()
+        scrollView.verticalScroller = nil
+        
+        let table = NSTableView()
+        table.headerView = nil
+        table.dataSource = navigatorDataSource
+        table.delegate = navigatorDelegate
+        
+        let column = NSTableColumn(identifier: NSUserInterfaceItemIdentifier(rawValue: "column"))
+        column.title = "Items"
+        table.addTableColumn(column)
+        
+        scrollView.documentView = table
+        scrollView.hasVerticalScroller = true
+        scrollView.hasHorizontalScroller = false
+        scrollView.autohidesScrollers = true
+        
+        table.reloadData()
+        
+        return scrollView
+    }
+    
+    private func makeList()-> NSView {
+        let scrollView = NSScrollView()
+        
+        let table = NSTableView()
+        table.headerView = nil
+        table.dataSource = tableDataSource
+        table.delegate = tableDelegate
+        
+        let column = NSTableColumn(identifier: NSUserInterfaceItemIdentifier(rawValue: "column"))
+        column.title = "Items"
+        table.addTableColumn(column)
+        
+        scrollView.documentView = table
+        scrollView.hasVerticalScroller = true
+        scrollView.hasHorizontalScroller = false
+        scrollView.autohidesScrollers = true
+        scrollView.borderType = .noBorder
+        
+        table.reloadData()
+        
+        return scrollView
+    }
+    
+    private func makeEditor()-> NSView {
+        let editor = NSStackView()
+        editor.spacing = 0
+        editor.orientation = .horizontal
+        
+        let input = NSTextView()
+        input.textContainerInset = NSSize(width: 8, height: 9)
+        input.font = NSFont.systemFont(ofSize: 14)
+        
+        let actionImage = NSImage(systemSymbolName: "paperplane.fill", accessibilityDescription: nil)
+        let action = NSImageView(image: actionImage!)
+        actionImage?.size = NSSize(width: ViewController.EDITOR_HEIGHT, height: ViewController.EDITOR_HEIGHT)
+        
+        editor.addArrangedSubview(input)
+        editor.addArrangedSubview(action)
+        
+        NSLayoutConstraint.activate([
+            input.topAnchor.constraint(equalTo: editor.topAnchor),
+            input.bottomAnchor.constraint(equalTo: editor.bottomAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            action.topAnchor.constraint(equalTo: editor.topAnchor),
+            action.bottomAnchor.constraint(equalTo: editor.bottomAnchor),
+            action.widthAnchor.constraint(equalToConstant: ViewController.EDITOR_HEIGHT),
+            action.heightAnchor.constraint(equalToConstant: ViewController.EDITOR_HEIGHT)
+        ])
+        
+        return editor
+    }
+    
 }
 
