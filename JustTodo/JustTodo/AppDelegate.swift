936d265708b470cb3b7005c80b294e3a00926d1a
boybeak <boybeak@gmail.com>
2024-07-10 20:56:58 +0800

    feat: add launch on boot

--- JustTodo/JustTodo/AppDelegate.swift
+++ JustTodo/JustTodo/AppDelegate.swift
@@ -8,6 +8,7 @@
 import AppKit
 import SwiftUI
 import Tray
+import LaunchAtLogin
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     
@@ -25,17 +26,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let menu = NSMenu()
                 
         //        Can not make launch on boot work until now
-        //        let launchBootItem = NSMenuItem(title: NSLocalizedString("menu_item_launch_on_boot", comment: "launch on boot menu item"), action: #selector(actionLaunchOnBoot(_:)), keyEquivalent: "")
-        //
-        //        launchBootItem.state = if bootLauncher.isLaunchAgentEnabled() {
-        //            .on
-        //        } else {
-        //            .off
-        //        }
-        //        launchBootItem.target = self
-        //        menu.addItem(launchBootItem)
-                
-        //        menu.addItem(.separator())
+        let launchBootItem = NSMenuItem(title: NSLocalizedString("menu_item_launch_on_boot", comment: "launch on boot menu item"), action: #selector(actionLaunchOnBoot(_:)), keyEquivalent: "")
+
+        launchBootItem.state = LaunchAtLogin.isEnabled ? .on : .off
+        
+        launchBootItem.target = self
+        menu.addItem(launchBootItem)
+        
+        menu.addItem(.separator())
         menu.addItem(NSMenuItem(title: NSLocalizedString("menu_item_about", comment: "About menu item"), action: #selector(actionAbout(_:)), keyEquivalent: ""))
         menu.addItem(NSMenuItem(title: NSLocalizedString("menu_item_quit", comment: "Quit menu item"), action: #selector(actionQuit(_:)), keyEquivalent: ""))
         tray.setMenu(menu: menu)
@@ -44,6 +42,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @objc func actionAbout(_ sender: NSMenuItem) {
         let aboutController = AboutWinController()
         aboutController.showWindow(nil)
+    }
+    
+    @objc func actionLaunchOnBoot(_ sender: NSMenuItem) {
+        LaunchAtLogin.isEnabled = !LaunchAtLogin.isEnabled
+        sender.state = LaunchAtLogin.isEnabled ? .on : .off
     }
 
     @objc func actionQuit(_ sender: NSMenuItem) {
